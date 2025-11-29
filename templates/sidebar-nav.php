64acc6d8329911ef548daf70a03a5e21cdcb0acb
Febri Suryanto <hello@febrisuryanto.com>
2025-11-29 18:55:59 +0700

    Update

---
+++ templates/sidebar-nav.php
@@ -0,0 +1,16 @@
+<div class="pos-sidebar">
+    <div class="pos-logo">
+        <img src="<?php echo KRESUBER_URL . 'assets/images/kresuber-logo.png'; ?>" alt="Kresuber Resto Logo">
+    </div>
+    <nav class="pos-nav-menu">
+        <ul>
+            <li><a href="#" class="active"><i class="ri-dashboard-line"></i> Dashboard</a></li>
+            <li><a href="#"><i class="ri-shopping-cart-2-line"></i> POS Terminal</a></li>
+            <li><a href="#"><i class="ri-table-line"></i> Table Management</a></li>
+            <li><a href="#"><i class="ri-restaurant-line"></i> Menu Management</a></li>
+            <li><a href="#"><i class="ri-file-list-3-line"></i> Orders</a></li>
+            <li><a href="#"><i class="ri-coupon-4-line"></i> Discounts</a></li>
+            <li><a href="#"><i class="ri-settings-4-line"></i> Settings</a></li>
+        </ul>
+    </nav>
+</div>
